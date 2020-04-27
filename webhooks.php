833f5d93373338cb317e72d0eb75efdb2b6d8bcd
Free-Ai <freeid6409@gmail.com>
2020-04-28 04:11:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,10 +23,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 
-  "type": "text",
-  "text": "สวัสดี"
-
-
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
