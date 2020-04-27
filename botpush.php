84743aca39e618f82324885873db8f9e09f0eaa7
Free-Ai <freeid6409@gmail.com>
2020-04-28 03:11:53 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,9 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = 'GuNN+eM0ofWFxHPXsa0JLxkM9n78AUghEzKUTu/qIg+JF1ko2ToXj9r3qduQTyZgMijCXmQtiwqMJC7Tj9wMNh/cJo3vUtYSy/JOP+2BKFaikSw2O1XPuRZ8HUBMy5XXd2biPGSe5FEU+5M3UrxMQgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '1654141442';
+$channelSecret = '8635a3c878452e4b79ea570dc7f5c841';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U430914f7dc0dba430c455e27c985d9aa';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
