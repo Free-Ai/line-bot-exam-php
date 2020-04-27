54057990e3f5b37e21fa2132afbaf393d3acf210
Free-Ai <freeid6409@gmail.com>
2020-04-28 03:53:38 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -49,14 +49,3 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-<?php
-require "vendor/autoload.php";
-$access_token = 'GuNN+eM0ofWFxHPXsa0JLxkM9n78AUghEzKUTu/qIg+JF1ko2ToXj9r3qduQTyZgMijCXmQtiwqMJC7Tj9wMNh/cJo3vUtYSy/JOP+2BKFaikSw2O1XPuRZ8HUBMy5XXd2biPGSe5FEU+5M3UrxMQgdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '8635a3c878452e4b79ea570dc7f5c841';
-$idPush = 'U430914f7dc0dba430c455e27c985d9aa'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
