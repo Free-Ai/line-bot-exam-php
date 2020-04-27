f7979ad4b730cf4f86727e87fd754df779ed8766
Free-Ai <freeid6409@gmail.com>
2020-04-27 12:41:00 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '';
+$access_token = '8635a3c878452e4b79ea570dc7f5c841';
 
 // Get POST body content
 $content = file_get_contents('php://input');
